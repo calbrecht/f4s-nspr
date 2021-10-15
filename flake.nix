eab85822bb9442569996cb3dc23f12e5461f4415
Christian Albrecht <christian.albrecht@mayflower.de>
2021-10-15 21:19:10 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "nspr nix flake.";
+
+  inputs = {
+    nspr-dev = {
+      # https://kuix.de/mozilla/versions/ NSPR: NSPR_4_32_RTM
+      url = https://hg.mozilla.org/projects/nspr/archive/NSPR_4_32_RTM.zip;
+      flake = false;
+    };
+    nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
+  };
+
+  outputs = { self, nixpkgs, nspr-dev }:
+    let
+      nspr_version = "4.32";
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages."${system}";
+    in
+    {
+      legacyPackages."${system}" = self.overlay self.legacyPackages."${system}" pkgs;
+
+      overlay = final: prev: {
+        nspr-dev = prev.nspr.overrideAttrs (old: {
+          version = nspr_version;
+          src = nspr-dev;
+          postUnpack = ''
+            mkdir nspr-${nspr_version}
+            mv $sourceRoot nspr-${nspr_version}/nspr
+            sourceRoot=nspr-${nspr_version}
+          '';
+        });
+      };
+    };
+}
